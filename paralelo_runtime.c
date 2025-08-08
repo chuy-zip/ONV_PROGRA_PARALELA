826dfa7d50078736d7aacfe5317c53ee1c93fb11
Eunice Mata <eunice.mata2992@gmail.com>
2025-08-07 22:38:32 -0600

    paralel runtime

---
+++ paralelo_runtime.c
@@ -0,0 +1,18 @@
+// paralelo_runtime.c
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    omp_set_num_threads(4);
+    int i;
+    int suma = 0;
+
+    #pragma omp parallel for schedule(runtime) reduction(+:suma)
+    for (i = 0; i < 16; i++) {
+        suma += i;
+        printf("Thread %d is running number %d\n", omp_get_thread_num(), i);
+    }
+
+    printf("Total: %d\n", suma);
+    return 0;
+}
