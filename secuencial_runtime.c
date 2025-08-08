564a932516070ccdf07f1a4bef7a74a6946b0d6f
Eunice Mata <eunice.mata2992@gmail.com>
2025-08-07 22:41:23 -0600

    secuencial runtime

--- secuencial_runtime.c
+++ secuencial_runtime.c
@@ -5,7 +5,7 @@ int main() {
     int suma = 0;
     for (i = 0; i < 16; i++) {
         suma += i;
-        printf("Thread %d, sparcial sum = %d\n", i, suma);
+        printf("Cicle %d, parcial sum = %d\n", i, suma);
     }
     printf("Suma total: %d\n", suma);
     return 0;
